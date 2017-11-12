2e0584ae566390bad9823ab36518219a0865ae60
Noah Stebbins <nstebbins@appnexus.com>
2017-11-12 18:40:33 -0500

    init

---
+++ src/main/java/Server.java
@@ -0,0 +1,108 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Server {
+
+    private List<Credential> credentials;
+    private Map<String, Queue<String>> messageQueue;
+    private Map<String, Queue<String>> blockedUsers;
+    private Map<String, Socket> userSockets;
+
+    public Server(List<Credential> credentials) {
+        this.credentials = credentials;
+        this.messageQueue = new ConcurrentHashMap<>();
+        this.blockedUsers = new ConcurrentHashMap<>();
+        this.userSockets = new ConcurrentHashMap<>();
+        for (Credential credential : this.credentials) {
+            this.messageQueue.put(credential.getUsername(), new ConcurrentLinkedQueue<>());
+            this.blockedUsers.put(credential.getUsername(), new ConcurrentLinkedQueue<>());
+            this.userSockets.put(credential.getUsername(), new Socket());
+        }
+    }
+
+    class ClientThread implements Runnable {
+
+        private Socket clientSocket;
+        private BufferedReader inFromClient;
+        private PrintWriter outToClient;
+
+        // TODO: add try/catch
+        public ClientThread(Socket clientSocket) throws Exception {
+            this.clientSocket = clientSocket;
+            this.inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            this.outToClient = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
+        }
+
+        // TODO: incorporate blacklist logic
+        public boolean greeting() throws Exception {
+            int numAttempts = 0;
+            boolean isValidated = false;
+            String username = null;
+            while (!isValidated && numAttempts < 3) {
+                // username
+                outToClient.println("username: ");
+                username = inFromClient.readLine();
+                // password
+                outToClient.println("password: ");
+                String password = inFromClient.readLine();
+                // auth
+                // TODO: also check if user is not already online
+                isValidated = credentials.contains(new Credential(username, password));
+                if (isValidated) {
+                    outToClient.println(ServerConstants.OK);
+                    // TODO: add to all necessary structures
+                    userSockets.putIfAbsent(username, clientSocket);
+                }
+                numAttempts++;
+            }
+            return false; // TODO: implement
+        }
+
+        @Override
+        public void run() {
+            try {
+                greeting();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static List<Credential> readCredentials(String file) {
+        List<Credential> credentials = new ArrayList<>();
+        try {
+            BufferedReader inFromFile = new BufferedReader(new FileReader(file));
+            while (inFromFile.ready()) {
+                String[] credentialList = inFromFile.readLine().split(" ");
+                credentials.add(new Credential(credentialList[0], credentialList[1]));
+
+            }
+            inFromFile.close();
+        } catch (Exception e) {
+            e.printStackTrace(); // TODO: better logging
+        }
+        return credentials;
+    }
+
+    public static void main(String args[]) throws Exception {
+        int port = 4000;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+        ServerSocket serverSocket = new ServerSocket(port);
+        Server newServer = new Server(readCredentials("src/main/user_pass.txt"));
+        // accept clients
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            Thread clientThread = new Thread(newServer.new ClientThread(clientSocket));
+            clientThread.start();
+        }
+    }
+}
---
+++ src/main/java/ServerConstants.java
@@ -0,0 +1,3 @@
+public final class ServerConstants {
+    public static final String OK = "welcome";
+}
