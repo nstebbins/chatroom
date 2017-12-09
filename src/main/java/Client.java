3cf3d57377a4cba6651a7ab86b7b4fd7b4afbbcc
Noah Stebbins <nstebbins@appnexus.com>
2017-12-09 15:48:06 -0500

    sending thread for client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,3 +1,4 @@
+import constants.ChatroomConstants;
 import greeting.ChatroomClientGreeting;
 import greeting.ClientGreeting;
 
@@ -11,7 +12,7 @@ public class Client {
     private BufferedReader inFromUser;
     private ClientGreeting clientGreeting;
 
-    public Client(Socket clientSocket) {
+    private Client(Socket clientSocket) {
         try {
             this.outToServer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
             this.inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -20,6 +21,23 @@ public class Client {
         }
         this.inFromUser = new BufferedReader(new InputStreamReader(System.in));
         this.clientGreeting = new ChatroomClientGreeting();
+    }
+
+    private class SendingThread implements Runnable {
+        public void run() {
+            try {
+                while (true) {
+                    String message = inFromUser.readLine();
+                    outToServer.println(message);
+                    if (message.equals(ChatroomConstants.LOGOUT)) {
+                        System.out.println("logout triggered client side");
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("error reading in user input");
+            }
+        }
     }
 
     public static void main(String args[]) throws Exception {
@@ -38,7 +56,11 @@ public class Client {
         // greet
         String username = client.clientGreeting.greet(client.inFromUser, client.inFromServer, client.outToServer);
         System.out.println("username: " + username);
+        // start threads
+        Thread send = new Thread(client.new SendingThread());
+        send.start();
         // clean-up
+        send.join();
         clientSocket.close();
     }
 }
--- src/main/java/constants/ChatroomConstants.java
+++ src/main/java/constants/ChatroomConstants.java
@@ -1,6 +1,9 @@
 package constants;
 
 public final class ChatroomConstants {
+    // states
     public static final String OK = "welcome";
     public static final String FAIL = "try again";
+    // commands
+    public static final String LOGOUT = "logout";
 }
